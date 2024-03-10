7dcf4897ce3956c5816dfdf876b676361c1bae8e
yuvikadhiman <yuvikadhiman25@gmail.com>
2024-03-11 02:57:36 +0530

    Home Page Update

--- client/src/components/DestinationCard.jsx
+++ client/src/components/DestinationCard.jsx
@@ -1,89 +1,80 @@
-import styled from 'styled-components';
-import { travelData } from '../utils/data';
-
+import styled from "styled-components";
+import { travelData } from "../utils/data";
+import { buses } from "../utils/data";
 const CardContainer = styled.div`
-  background-color: white;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 1rem;
-  margin: 20px 3%;
-`;
-
-const CardGrid = styled.div`
+  margin-top: 10px;
+  background-color: #ffffff;
+  padding: 40px 20px;
+  width: 780px;
+  box-shadow: 0 1px 5px 0 rgba(0, 0, 0, 0.1);
+  border-radius: 6px;
   display: grid;
-  width: 100%;
-  grid-template-columns: repeat(4, 1fr);
-  gap: 1rem;
-  justify-items: center;
-  margin: 0 auto;
-
-  @media (max-width: 1280px) {
-    grid-template-columns: repeat(3, 1fr);
-  }
-  @media (max-width: 780px) {
-    grid-template-columns: repeat(2, 1fr);
-  }
-
-  @media (max-width: 480px) {
-    grid-template-columns: 1fr;
-  }
-`;
-
-const Card = styled.div`
-  min-width: 200px;
-  background-color: white;
-  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
-  width: 80%;
-  padding: 5%;
-  border-radius: 10px;
-`;
-
-const CardImage = styled.img`
-  border-radius: 10px;
-  width: 100%;
-`;
-
-const CardContent = styled.div`
-  display: flex;
-  flex-direction: column;
+  grid-template-columns: 1fr 1fr;
   align-items: center;
-  gap: 10px;
-`;
 
-const CardTitle = styled.h2`
-  font-size: 1.5rem;
 `;
-
-const CardButton = styled.button`
-  background-color: #ffc700;
-  color: white;
-  font-weight: bold;
-  border: none;
-  padding: 10px 20px;
-  text-decoration: none;
-  border-radius: 5px;
-  cursor: pointer;
-  &:hover {
-    background-color: var(--hoverBtn);
+const ServiceProvider = styled.div`
+  p:nth-child(1) {
+    font-size: 18px;
+    color: #5e6fa2;
+    font-weight: 600;
+    padding: 5px 0;
+  }
+  & {
+    font-size: 16px;
+  }
+  span {
+    font-weight: 500;
+  }
+`;
+const ServiceProviderDetails = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  color: #000000;
+  gap: 1rem;
+  button {
+    border: none;
+    cursor: pointer;
+    width: 100px;
+    color: white;
+    background-color: rgb(221, 20, 50);
+    border-radius: 4px;
+    padding: 8px 5px;
+    margin-top: 1rem;
   }
 `;
 
 const DestinationCard = () => {
   return (
-    <CardContainer>
-      <CardGrid>
-        {travelData.map((data) => (
-          <Card key={data.id}>
-            <CardContent>
-              <CardImage src={data.image} alt="Destination" />
-              <CardTitle>{data.text}</CardTitle>
-              <CardButton>Book Now</CardButton>
-            </CardContent>
-          </Card>
-        ))}
-      </CardGrid>
-    </CardContainer>
+    <>
+      <>
+        {buses.map((item) => {
+          return (
+            <CardContainer>
+              <ServiceProvider>
+                <p>{item?.name}</p>
+                <p>
+                  <span>Route : </span>
+                  {item?.route}
+                </p>
+                <p>
+                  <span>Boarding Point : </span>
+                  {item?.boardingPoint}
+                </p>
+              </ServiceProvider>
+              <ServiceProviderDetails>
+                <p>{item?.departureTime}</p>
+                <p>{item?.arrivalTime}</p>
+                <p>
+                  {item?.price}
+                  <button>Book my pass</button>
+                </p>
+              </ServiceProviderDetails>
+            </CardContainer>
+          );
+        })}
+      </>
+    </>
   );
 };
 
